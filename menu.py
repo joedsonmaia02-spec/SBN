58188ba6801dc0a713d529f44d9093f6c0f1f30e
Joedson Maia <joedsonmaia02@gmail.com>
2026-08-13 21:40:38 -0300

    Modificação MENU e na codificação do Morse
    Adicionado While no Menu e está "a prova de falhas"

--- menu.py
+++ menu.py
@@ -1,38 +1,55 @@
-print("======= Programa de Cifragem e Decifragem =======")
-entrada = str(input("1. Cifrar \n 2. Decifrar \n "))
-if entrada == "1":
-    tipocifra = str(input("Escolha o tipo de cifra: \n A: Cifra de César \n B: Cifra de Vigenère \n C: Cifra de Atbash \n D: Morse \n E: Ascii \n"))
-    if tipocifra == "A" or "a":
-        cesarmsg = str(input("Informe a mensagem que deseja cifrar:"))
+from codificacao_morse import codifica_morse
 
-    elif tipocifra == "B" or "b":
-        cesarmsg = str(input("Informe a mensagem que deseja cifrar:"))
 
-    elif tipocifra == "C" or "c":
-        cesarmsg = str(input("Informe a mensagem que deseja cifrar:"))
-    elif tipocifra == "D" or "d":
-        cesarmsg = str(input("Informe a mensagem que deseja cifrar:"))
-    elif tipocifra == "E" or "e":
-        cesarmsg = str(input("Informe a mensagem que deseja cifrar:"))
-    else:
-        print("Esta não é uma entrada válida.")
+while 1:
+    entrada = str(input("======= Programa de Cifragem e Decifragem ======= \n  1. Cifrar \n 2. Decifrar \n  3. Sair \n"))
+    if entrada=="1":
+        tipocifra = str(input("Escolha o tipo de cifra: \n A: Cifra de César \n B: Cifra de Vigenère \n C: Cifra de Atbash \n D: Morse \n E: Ascii \n")).upper()
 
-elif entrada == "2":
-  tipocifra = str(input("Escolha o tipo de cifra: \n A: Cifra de César \n B: Cifra de Vigenère \n C: Cifra de Atbash \n D: Morse \n E: Ascii \n"))
-  if tipocifra == "A" or "a":
-          cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
+        if tipocifra == "A":
+            cesarmsg = str(input("Informe a mensagem que deseja cifrar:"))
 
-  elif tipocifra == "B" or "b":
-          cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
 
-  elif tipocifra == "C" or "c":
-          cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
-  elif tipocifra == "D" or "d":
-          cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
-  elif tipocifra == "E" or "e":
-          cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
-  else:
-          print("Esta não é uma entrada válida.")
+        elif tipocifra == "B":
+            cesarmsg = str(input("Informe a mensagem que deseja cifrar:"))
 
-else:
-  print("Esta não é uma entrada válida.")
+
+        elif tipocifra == "C":
+            cesarmsg = str(input("Informe a mensagem que deseja cifrar:"))
+
+        elif tipocifra == "D":
+            codifica_morse()
+
+        elif tipocifra == "E":
+            cesarmsg = str(input("Informe a mensagem que deseja cifrar:"))
+
+        else:
+            print("Esta não é uma entrada válida.")
+            print("Selecione uma um dos abaixo: ")
+            tipocifra = str(input("Escolha o tipo de cifra: \n A: Cifra de César \n B: Cifra de Vigenère \n C: Cifra de Atbash \n D: Morse \n E: Ascii \n")).upper()
+
+    elif entrada == "2":
+        tipocifra = str(input("Escolha o tipo de cifra: \n A: Cifra de César \n B: Cifra de Vigenère \n C: Cifra de Atbash \n D: Morse \n E: Ascii \n")).upper()
+
+        if tipocifra == "A":
+            cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
+
+        elif tipocifra == "B":
+            cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
+
+        elif tipocifra == "C":
+            cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
+
+        elif tipocifra == "D":
+            cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
+
+        elif tipocifra == "E":
+            cesarmsg = str(input("Informe a mensagem que deseja decifrar:"))
+
+        else:
+            print("Selecione um tipo de cifra válido ")
+            str(input("Escolha o tipo de cifra: \n A: Cifra de César \n B: Cifra de Vigenère \n C: Cifra de Atbash \n D: Morse \n E: Ascii \n")).upper()
+
+    elif entrada=="3":
+        print("="*20,"\n","Sistema encerrado","\n","="*20)
+        break
